503e4ddd08ca97be3ebbdfc122a541b65c4a6e34
Your Name <you@example.com>
2023-04-11 14:00:57 +0530

    Programs using If-Else statement

---
+++ ProgrammingConstruct/IfElse.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    public class IfElse 
+    {  
+        // checking two integers are equal or not using if-else statement
+        public static void Compare()
+        {
+              Console.WriteLine("enter an integer");
+              int num1 = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("enter an integer");
+              int num2 = Convert.ToInt32(Console.ReadLine());
+
+              if (num1 == num2)
+              {
+                 Console.WriteLine("Both integers are equal");
+              }
+              else
+              {
+                  Console.WriteLine("Both integers are not equal");
+              }
+        }
+
+        // checking a number is even or odd using if-else statement
+
+        public static void Check()
+        {
+            Console.WriteLine("enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("{0} is even number", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is odd number", number);
+            }
+        }
+
+        // checking whether a candidate is elgibile for vote or not using if-else statement
+
+        public static void CheckElgibilty()
+        {
+            Console.WriteLine("enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("you are elgible for vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not elgible for vote");
+            }
+        }
+
+    }
+}
+        
+    
+
---
+++ ProgrammingConstruct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProgrammingConstruct
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IfElse.Compare();
+            IfElse.Check();
+            IfElse.CheckElgibilty();
+
+            Console.WriteLine();
+        }
+    }
+}
